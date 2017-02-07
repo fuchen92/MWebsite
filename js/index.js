6af98af037cf01c388e2ecb373328b0a67aaea47
浮尘 <engels@traveldaily.cn>
2017-02-07 11:37:03 +0800

    此处的js写到了html文件下面

--- js/index.js
+++ js/index.js
@@ -11,16 +11,74 @@ $(document).ready(function () {
     });
     $(".header-search").on("click", function () {
         switchSiderbar(".siderbar-search", ".search-arrow, .search-cancel", "siderbar-search-active");
+        $("#searchInput").focus();
     });
+
+    var navMenu = new Swiper(".nav-menu-list", {
+        spaceBetween: 10,
+        slidesPerView: "auto",
+        freeMode: true
+    });
+
+    
+    var carousel1 = new Swiper(".carousel-one", {
+        spaceBetween: 20,
+        // width: "auto",
+        slidesPerView: "1.6",
+        onSlideChangeEnd: function(swiper){
+            var currentIndex = swiper.activeIndex;
+            var carouselItem = $(".carousel-one .carousel-item")
+            // // console.log(currentIndex) //切换结束时，告诉我现在是第几个slide
+            if (currentIndex >= carouselItem.length - 2 ) {
+                console.log("到底了")
+            }
+            // console.log(swiper);
+            // if (swiper.isEnd) {
+            //     console.log("已经到最后了")
+            // }
+        }
+    });
+
+    // $(".news-category-title").on("click", function () {
+    //     console.log(carousel.activeIndex)
+    // });
+    // var swiperWrapper = $(".swiper-wrapper");
+    // var moveWidth = $(".carousel-item").width() + 15;
+    // var num = 0;
+    // var initX = 0;
+    // $(".carousel").on("touchstart", function (event) {
+    //     var event = window.event || event;
+    //     event.stopPropagation();
+    //     initX = event.touches[0].pageX;
+    //     num++;
+    // });
+
+    // $(".carousel").on("touchmove", function (event) {
+    //     console.log(num)
+    //     var event = window.event || event;
+    //     event.preventDefault();
+    //     var moveX = event.touches[0].pageX;
+    //     var x = moveX - initX;
+    //     if (x < 0) {
+    //         console.log(num)
+    //         swiperWrapper.animate({
+    //             "left": -moveWidth * num
+    //         }, 200)
+    //     }
+    // });
+    // $(".carousel").on("touchend", function (event) {
+    //     var event = window.event || event;
+    //     // alert(111)
+    // });
 
     // $(document).on("touchstart", function (event) {
     //     var event = window.event || event;
-    //     var initX = event.targetTouches[0].clientX,
-    //         initY = event.targetTouches[0].clientY;
+    //     var initX = event.touches[0].clientX,
+    //         initY = event.touches[0].clientY;
     //     $(document).on("touchmove", function (event) {
     //         var event = window.event || event;
-    //         var moveX = event.targetTouches[0].clientX,
-    //             moveY = event.targetTouches[0].clientY;
+    //         var moveX = event.touches[0].clientX,
+    //             moveY = event.touches[0].clientY;
 
     //         var navs = $("nav, .footer-nav-container"),
     //             footer = $(".footer");
@@ -31,10 +89,25 @@ $(document).ready(function () {
     //         } else {
     //             footer.css("margin-bottom", "0");
     //             navs.css("display", "none");
-    //             console.log(footer)
     //         }
     //     })
     // });
+
+    var subscribeInput = $(".subscribe-input"),
+        tipsContainer = $(".tips-container");
+    $("#subscribeBtn").on("click", function () {
+        if (subscribeInput.val() == "") {
+            subscribeInput.focus();
+            tipsContainer.text("请输入您的邮箱地址").addClass("tips-container-active");
+            return false;
+        } else if (!/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test( subscribeInput.val() )) {
+            subscribeInput.focus();
+            tipsContainer.text("您的邮箱格式不正确，请检查").addClass("tips-container-active");
+        }
+    });
+    subscribeInput.on("blur", function () {
+        tipsContainer.removeClass("tips-container-active");
+    });
 
     $(".nav-menu-item").on("click", function () {
         switchMenu($(this), "nav-active-menu");
@@ -46,4 +119,4 @@ $(document).ready(function () {
     function switchMenu (targetEle, classType) {
         targetEle.addClass(classType).siblings().removeClass(classType);
     }
-});
+})
